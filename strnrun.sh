2cd03b281b6c7272a9fa9b1effd330853a7de408
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2022-03-22 18:25:04 +0530

    Update strnrun.sh

--- strnrun.sh
+++ strnrun.sh
@@ -1,4 +1,4 @@
 read -p "Enter the bssid of the hacking wifi-network: " bssid
 read -p "Enter the channel: " channel 
-mkdir output
+sudo mkdir output
 sudo airodump-ng -w output/hack1 -c $channel --bssid $bssid wlan0
